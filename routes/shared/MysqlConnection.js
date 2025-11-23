3c2056c645998e8a7b6c5328126b4cc3fd5cfb61
techsynergysolutions2-sys <techsynergysolutions2@gmail.com>
2025-11-23 19:32:36 +0200

    Added mysql connect to RDS

--- routes/shared/MysqlConnection.js
+++ routes/shared/MysqlConnection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Junior980417",
+    host: "crm-main-database.c70iuwmoi6p8.eu-north-1.rds.amazonaws.com",
+    user: "admin",
+    password: "CnIghOFNoFtE5ylWR0Qy",
     database: "crm"
 })
 
