31c65fa0a59c15ec1a1a6992dc92aa1295c8a3d1
techsynergysolutions2-sys <techsynergysolutions2@gmail.com>
2025-11-24 19:37:54 +0200

    commit

--- routes/shared/MysqlConnection.js
+++ routes/shared/MysqlConnection.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2/promise');
 
-// const db = mysql.createPool({
-//     host: "crm-main-database.c70iuwmoi6p8.eu-north-1.rds.amazonaws.com",
-//     user: "admin",
-//     password: "CnIghOFNoFtE5ylWR0Qy",
-//     database: "crm"
-// })
-
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Junior980417",
+    host: "crm-main-database.c70iuwmoi6p8.eu-north-1.rds.amazonaws.com",
+    user: "admin",
+    password: "CnIghOFNoFtE5ylWR0Qy",
     database: "crm"
 })
+
+// const db = mysql.createPool({
+//     host: "localhost",
+//     user: "root",
+//     password: "Junior980417",
+//     database: "crm"
+// })
 
 module.exports = db;
